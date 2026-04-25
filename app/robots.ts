adac7728ec9c4eae4e6a69c9ca85a91e5b6b4a95
roor330-collab <roor330@gmail.com>
2026-04-25 20:47:35 +0200

    Add app/robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/admin",
+    },
+    sitemap: "https://eventrip.com/sitemap.xml",
+  };
+}
